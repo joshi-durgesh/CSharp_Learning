406be2501e64f925507bbe83502b20d2dabe2d80
joshi-durgesh <durgesh7joshi9@gmail.com>
2024-11-08 14:58:58 +0530

    Start making Todo application

--- First_Code/Program.cs
+++ First_Code/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("What do you want to do.");
+Console.WriteLine("[S]ee all TODO's");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
