b03716132c238714d28840d7e11b9e9f809c7ebe
Durgesh Kumar Joshi <154745524+joshi-durgesh@users.noreply.github.com>
2024-11-11 19:42:57 +0530

    String Interpolation and String Concatnation

--- First_Code/Program.cs
+++ First_Code/Program.cs
@@ -1,4 +1,5 @@
 ﻿// To make a TODO list
+/*
 Console.WriteLine("Hello!");
 
 // Reference Type
@@ -16,3 +17,21 @@ System.Console.WriteLine($"Pet name is {petName}");
 petName = "Doggy2";
 
 Console.WriteLine($"My secong pet name is {petName}");  
+
+*/
+
+// Defining Variable for 2 numbers
+int numFirst;
+int numSecond;
+
+// Taking input number from user
+Console.WriteLine("Enter First Number:");
+string inputNumFirst = Console.ReadLine();
+Console.WriteLine("Enter Second Number");
+string inputNumSecond = Console.ReadLine();
+
+// Initializing two input numbers and parse it to int
+numFirst = int.Parse(inputNumFirst);
+numSecond = int.Parse(inputNumSecond);
+
+Console.WriteLine($"The sum of {numFirst} and {numSecond} is {numFirst + numSecond}");
