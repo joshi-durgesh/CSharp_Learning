5874d7c40febafe93ac123356a05f9bd3dfd3718
Durgesh Kumar Joshi <154745524+joshi-durgesh@users.noreply.github.com>
2024-11-14 10:08:49 +0530

    Bool Datatype

--- Making_Decison/Program.cs
+++ Making_Decison/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Decisions Making
+
+//Bool Datatype
+
+bool isRainy = false;
+bool haveUmbrella = true;
+
