985bfe8364e439941bc2616aadc84f47a4eab869
Durgesh Kumar Joshi <154745524+joshi-durgesh@users.noreply.github.com>
2024-11-14 13:31:41 +0530

    TryParse

--- Making_Decison/Program.cs
+++ Making_Decison/Program.cs
@@ -60,3 +60,20 @@ switch (day)
         break;
 }
 */
+
+// TryParse
+
+Console.WriteLine("Enter a number to check whether it is Even or Odd");
+int num;
+bool isNum = int.TryParse(Console.ReadLine(), out num);
+
+if(isNum){
+    if(num%2 == 0){
+        Console.WriteLine("The number {0} is even.",num);
+    }
+    else{
+        Console.WriteLine("The number {0} is odd.",num);
+    }
+} else{
+    Console.WriteLine("Please enter whole number only");
+}
