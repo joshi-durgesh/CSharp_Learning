4a08fd4ea99ba96028d1acfb8b935dea4eb38744
Durgesh Kumar Joshi <154745524+joshi-durgesh@users.noreply.github.com>
2024-11-14 15:34:10 +0530

    Building Conditional based calculator

--- Making_Decison/Program.cs
+++ Making_Decison/Program.cs
@@ -59,7 +59,6 @@ switch (day)
         Console.WriteLine("Another day");
         break;
 }
-*/
 
 // TryParse
 
@@ -76,4 +75,43 @@ if(isNum){
     }
 } else{
     Console.WriteLine("Please enter whole number only");
+}
+*/
+
+
+//Build a conditiona based calculator
+// prompt 2 input numbers
+// prompt for arithmetic operation
+// handle division by 0
+
+double num1;
+double num2;
+string operation = "";
+
+Console.WriteLine("Enter the first number:");
+bool isNum1 = double.TryParse(Console.ReadLine(), out num1);
+Console.WriteLine("Enter the second number:");
+bool isNum2 = double.TryParse(Console.ReadLine(), out num2);
+
+if( isNum1 && isNum2){
+    Console.WriteLine("Choose an operation (+, -, * or /):");
+    operation = Console.ReadLine();
+    if(operation.Trim() == "+"){
+        Console.WriteLine($"Result: {num1 + num2}");
+    } else if(operation.Trim() == "-"){
+        Console.WriteLine($"Result: {num1 - num2}");
+    }else if(operation.Trim() == "*"){
+        Console.WriteLine($"Result: {num1 * num2}");
+    }else if(operation.Trim() == "/"){
+        if(num2 != 0){
+        Console.WriteLine($"Result: {num1 / num2}");
+        } else{
+            Console.WriteLine("You can not devide a number with 0.");
+        }
+    } else{
+        Console.WriteLine("Enter a valid operation.");
+    }
+
+} else{
+    Console.WriteLine("Please enter a valid number");
 }
