ac7d002434b89439ed8e88ffe86d5a66986c037d
Durgesh Kumar Joshi <154745524+joshi-durgesh@users.noreply.github.com>
2024-11-14 10:59:26 +0530

    Logical Operators

--- Making_Decison/Program.cs
+++ Making_Decison/Program.cs
@@ -10,3 +10,25 @@ bool haveUmbrella = true;
 if(isRainy){
     Console.WriteLine("It's rainy.");
 }
+
+// Logical Operators
+
+// AND &&
+// True && True -> True
+// True && False -> False
+// False && True -> False
+// Fasle && False -> False
+
+// OR || 
+// True || True -> True
+// True || False -> True
+// False || True -> True
+// Fasle || False -> False
+
+// NOT !
+// !True -> False
+// !False -> True
+
+if (!isRainy || haveUmbrella){
+    Console.WriteLine("I'm not getting Wet.");
+}
