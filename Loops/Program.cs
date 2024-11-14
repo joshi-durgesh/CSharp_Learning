ec5cb81692a8cc350c78258381aadff74876eea8
Durgesh Kumar Joshi <154745524+joshi-durgesh@users.noreply.github.com>
2024-11-14 22:15:07 +0530

    rocket landing console app with use of Thread.Sleep() and for loop and escape character

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -8,10 +8,21 @@ for (int counter = 10; counter > 0; counter--)
 {
     Console.WriteLine("Negative Counter " + counter);
 }
-*/
 // counter
 
 for (int counter = 0; counter <= 10; ++counter)
 {
     Console.WriteLine("Counter "+ counter);
+}
+*/
+
+//Rocket simulation
+string rocket = "\r\n       |\r\n      ---\r\n     /   \\\r\n     |.o.|\r\n     _---_\r\n    //,,,\\\\\n";
+
+for(int count = 5; count >=0; count--){
+    Console.Clear();
+    Console.WriteLine("Countdown " + count);
+    Console.WriteLine(rocket = "\r\n" + rocket);
+    if(count==0)Console.WriteLine("Rocket Landed Sucessfuly");
+    Thread.Sleep(1000);
 }
