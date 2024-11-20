dfde5fc02e185344b60ae45b59d94fabad674055
Durgesh Kumar Joshi <154745524+joshi-durgesh@users.noreply.github.com>
2024-11-20 10:07:27 +0530

    function / methods simple void method with or without parameter

---
+++ Function/Program.cs
@@ -0,0 +1,21 @@
+﻿// Function / Methods
+// Methods are structured like this
+// Modifier returnType MethodName (Parameter){
+// Code Block
+//}
+
+// simple method 
+void PrintHello(){
+    Console.WriteLine("Hello World!");
+}
+
+// PrintHello();
+
+// Simple void method with parameter
+void PrintBio(string name,int age, string gender){
+    Console.WriteLine($"Hello, my name is {name} and i am {age} year's old {gender}");
+}
+
+int age = 24;
+
+PrintBio("Durgesh",age,"Male");
