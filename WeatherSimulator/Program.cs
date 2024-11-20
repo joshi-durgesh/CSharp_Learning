768641c7de720601b7fdbe0999d34feba41ae4cc
Durgesh Kumar Joshi <154745524+joshi-durgesh@users.noreply.github.com>
2024-11-20 16:16:43 +0530

    WeatherSimulator program that take input no of days and prints the weather datails of those no of days

--- WeatherSimulator/Program.cs
+++ WeatherSimulator/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("Enter the number of days: ");
+        int days = int.Parse(Console.ReadLine());
+
+        int[] temprature = new int[days];
+        string[] conditions = { "Sunny", "Cloudy", "Rainy", "Cold", };
+        int[] weatherConditions = new int[days];
+
+        Random random = new Random();
+
+        for (int i = 0; i < days; i++)
+        {
+            temprature[i] = random.Next(-10, 44);
+            if (temprature[i] < 10)
+            {
+                weatherConditions[i] = 3;
+                continue;
+            }
+            weatherConditions[i] = random.Next(conditions.Length);
+        }
+        // Console.WriteLine("Hello, World!");
+
+        for (int i = 0; i < days; i++)
+        {
+            Console.WriteLine($"Wheather condition at day {i + 1} is {conditions[weatherConditions[i]]} with temprature of {temprature[i]}");
+        }
     }
 }
