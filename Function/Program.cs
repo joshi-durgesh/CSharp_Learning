ad7b6e6fdd9bde782806e26decefdad07addb541
Durgesh Kumar Joshi <154745524+joshi-durgesh@users.noreply.github.com>
2024-11-20 10:17:18 +0530

    method with return value

--- Function/Program.cs
+++ Function/Program.cs
@@ -18,4 +18,19 @@ void PrintBio(string name,int age, string gender){
 
 int age = 24;
 
-PrintBio("Durgesh",age,"Male");
+// PrintBio("Durgesh",age,"Male");
+
+// Method that return something
+
+int AdditionCalculator(int num1, int num2){
+    int result = num1 + num2;
+    return result;
+}
+Console.WriteLine("Enter 1st number:");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter 2nd number:");
+int num2 = int.Parse(Console.ReadLine());
+
+int sum =AdditionCalculator( num1, num2);
+Console.Write("The sum of the {0} and {1} is :",num1,num2);
+Console.WriteLine(sum);
