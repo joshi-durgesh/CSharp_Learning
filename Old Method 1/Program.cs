081ae7a15cbe5b9cd7e9361aff2a470d78927aa5
Durgesh Kumar Joshi <154745524+joshi-durgesh@users.noreply.github.com>
2024-11-20 12:21:38 +0530

    Creating method outside the main method and accessing it

--- Old Method 1/Program.cs
+++ Old Method 1/Program.cs
@@ -4,6 +4,28 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Taking inputs from user
+        Console.Write("Enter first number: ");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.Write("Enter second number: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int addition = Addition(num1, num2);
+        Console.WriteLine("The sum of the {0} and {1} is {2}. ",num1,num2,addition);
+
+        // Creating instance of program class
+        Program myProgram = new Program(); 
+
+        // Accessing Substraction with the help of program instance we created    
+        int substraction = myProgram.Substraction(num1, num2);
+        Console.WriteLine("The minus of the {0} and {1} is {2}. ",num1,num2,substraction);
+    }
+
+    static int Addition(int num1, int num2){
+        return num1 + num2;
+    }
+
+    int Substraction(int num1, int num2){
+        return num1 - num2; 
     }
 }
