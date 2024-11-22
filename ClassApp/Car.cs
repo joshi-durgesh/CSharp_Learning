9bcef6be461c4701634db2476c035ed7fb647965
Durgesh Kumar Joshi <154745524+joshi-durgesh@users.noreply.github.com>
2024-11-22 13:02:33 +0530

    Member variable to create and naming conventions

--- ClassApp/Car.cs
+++ ClassApp/Car.cs
@@ -7,11 +7,19 @@ namespace ClassApp
 {
     public class Car
     {
+
+        // Member Variable
+        // private hides the variable from other classes
+        private string _model = "";
+        private string _brand = "";
+
         // ! Constructor
         // Constructor is called every time when class instance is created
-        public Car()
+        public Car(string brand, string model)
         {
-            Console.WriteLine("Car Object Instance has been created.");
+            _brand = brand;
+            _model = model;
+            Console.WriteLine($"The {_brand} car of model {_model} has been created.");
         }
 
     }
--- ClassApp/Program.cs
+++ ClassApp/Program.cs
@@ -5,8 +5,9 @@ class Program
     static void Main(string[] args)
     {
         // Creating Instance of Car class
-        Car car = new Car();
+        Car audi = new Car("Audi", "A3");
+        Car bmw = new Car("BMW", "i7");
 
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, from main program.");
     }
 }
