ecb6391bc1155571b2a9054f6545587bf3833037
Durgesh Kumar Joshi <154745524+joshi-durgesh@users.noreply.github.com>
2024-11-28 13:34:40 +0530

    Collection initial List

---
+++ Collections/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Collections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = new List<int>();
+        numbers.Add(1);
+        numbers.Add(2);
+
+    }
+}
