e2fc5659354fc074bd1e68d0aa9eaada8053acfb
Durgesh Kumar Joshi <154745524+joshi-durgesh@users.noreply.github.com>
2024-12-27 13:00:50 +0530

    if else practice

--- Practice/Practice/Program.cs
+++ Practice/Practice/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int num1 = 23;
+        float num2 = 1.3f;
+        double num3 = 23.3;
+        char grade = 'A';
+        bool isPassed = false;
+        string firstName = "Durgesh";
+        string lastName = "Joshi";
+
+        Console.WriteLine($"My name is {firstName} {lastName}");
+        if (isPassed)
+        {
+            Console.WriteLine("Passed with {grade}.");
+
+        }
+        else
+        {
+            Console.WriteLine("Failed and have to study again the same.");
+        }
     }
 }
